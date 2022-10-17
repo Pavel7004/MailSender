6823b13edf8dfd96ac840538bf75349ed9296b1a
Kovalev Pavel <kovalev5690@gmail.com>
2022-10-17 22:01:20 +0300

    Add main func

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"github.com/rs/zerolog/log"
 
+	"github.com/Pavel7004/MailSender/pkg/adapter/db/mongo"
+	"github.com/Pavel7004/MailSender/pkg/infra/config"
+)
+
+func main() {
+	cfg, err := config.Get()
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to read config")
+		return
+	}
+
+	db, err := mongo.New(cfg)
+	defer func() {
+		if err := db.Close(); err != nil {
+			log.Error().Err(err).Msg("Failed to close connection to db")
+		}
+	}()
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to connect to DB")
+		return
+	}
 }
